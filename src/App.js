c8ef1156205db43828c2f214f829f2c6ea1ea676
nazm0l <tanjirahmednazmul@gmail.com>
2022-05-23 02:50:13 +0600

    Navbar added

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./Pages/Home/Home";
 import Login from "./Pages/Login/Login";
+import Navbar from "./Pages/Shared/Navbar";
 
 function App() {
   return (
     <div className="container mx-auto">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="login" element={<Login />} />
--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -1,10 +1,69 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+
+    const menuItems = <>
+    <li>
+     <Link to="/">Home</Link>
+   </li>
+   <li>
+     <Link to="/shop">Shop</Link>
+   </li>
+   <li>
+     <Link to="/about">About</Link>
+   </li>
+   <li>
+     <Link to="/contact">Contact Us</Link>
+   </li>
+   <li>
+     <Link to="/login">LogIn</Link>
+   </li>
+</>
+
+
     return (
-        <div>
-            <h2 className="text-2xl">Nav</h2>
+        <div className="navbar container mx-auto bg-base-100 font-semibold">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex="0" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex="0"
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+          >
+           {menuItems}
+          </ul>
         </div>
+        <Link to="/" className="btn btn-ghost normal-case text-xl">
+          iParts
+        </Link>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal p-0">
+        {menuItems}
+        </ul>
+      </div>
+      <div className="navbar-end">
+        <Link to="/" className="btn btn-secondary">
+          Get started
+        </Link>
+      </div>
+    </div>
     );
 };
 
